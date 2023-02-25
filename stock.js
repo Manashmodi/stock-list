2609c69b048f505c2b3044df8341cdd972b3c91d
Manashmodi <86552684+Manashmodi@users.noreply.github.com>
2023-02-25 16:58:49 +0530

    Add files via upload

--- stock.js
+++ stock.js
@@ -1,26 +1,38 @@
-function getStocks(){
-    var url="http://localhost:7600/stocks";
-    var url2 = "https://priceapi.moneycontrol.com/pricefeed/bse/equitycash"
-    fetch(url)
+
+
+function getStock() {
+    var url = "http://localhost:4500/stocks";
+    var url2="https://priceapi.moneycontrol.com/pricefeed/bse/equitycash";
+  fetch(url)
     .then((res) => res.json())
     .then((data) => {
-        data.map((api1) => {
-            fetch(`${url2}/${api1.name}`)
-            .then((res) => res.json())
-            .then((api2) => {
-                var element = `
-                    <div class="card" style="width:25%;height:450px;float:left;margin-left:2%;margin-top:2%">
-                        <img src='${api1.logo}' class="card-img-top" style="height:250px;"/>
-                        <div class="card-body">
-                            <h5 class="card-title">${api2.data.company}</h5>
-                            <h5>${api2.data.HN}-${api2.data.SC_SUBSEC}</h5>
-                            <p>Price Current:${api2.data.pricecurrent}</p>
-                            <p>Price Change: ${api2.data.pricechange}</p>
-                            <p>Price Percent Change: ${api2.data.pricepercentchange}%</p>
-                        </div>
-                    </div>`
-                $('#display').append(element);
-            })
-        })
-    })
+        data.map((item) => {
+          fetch(`${url2}/${item.name}`)
+          .then((res) => res.json)
+          .then((api2) =>{
+            var element = `
+            <div class="card" style="width:25%;height:450px;float:left;margin-left:2%;margin-top:2%">
+                <img src='${api1.logo}' class="card-img-top" style="height:250px;"/>
+                <div class="card-body">
+                    <h5 class="card-title">${api2.data.company}</h5>
+                    <h5>${api2.data.HN}-${api2.data.SC_SUBSEC}</h5>
+                    <p>Price Current:${api2.data.pricecurrent}</p>
+                    <p>Price Change: ${api2.data.pricechange}</p>
+                    <p>Price Percent Change: ${api2.data.pricepercentchange}%</p>
+                </div>
+            </div>`
+        $('#display').append(element);
+          });
+        });
+    });
 }
+
+
+/*data.map((item) => {
+    var element = document.createElement("li");
+    var text = document.createTextNode(item.name);
+    element.appendChild(text);
+    var main = document.getElementById("slist");
+    main.appendChild(element);
+  });
+  */
